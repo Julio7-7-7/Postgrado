208a84ceb1da276118bbe4a8887de2e530c5eed4
Julio7-7-7 <julio.toledo2030@gmail.com>
2026-04-21 22:17:33 -0400

    refactor: subiendo de nivel los archivos del proyecto y cambiando de nombre al repo

---
+++ migrations/versions/083a5c4e844a_fetch_agregando_tabla_programa.py
@@ -0,0 +1,36 @@
+"""fetch: agregando tabla programa
+
+Revision ID: 083a5c4e844a
+Revises: 08ccdffc578a
+Create Date: 2026-04-20 22:42:39.737878
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '083a5c4e844a'
+down_revision: Union[str, Sequence[str], None] = '08ccdffc578a'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # ### Borra el bloque de op.create_table('tipo_programa', ...) y su índice ###
+    op.create_table('programa',
+        sa.Column('id_programa', sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column('id_tipo_programa', sa.Integer(), nullable=False),
+        sa.Column('nombre_programa', sa.String(), nullable=False),
+        sa.Column('vigente', sa.Boolean(), nullable=False),
+        sa.ForeignKeyConstraint(['id_tipo_programa'], ['tipo_programa.id_tipo_programa'], ),
+        sa.PrimaryKeyConstraint('id_programa')
+    )
+    op.create_index(op.f('ix_programa_id_programa'), 'programa', ['id_programa'], unique=False)
+
+def downgrade() -> None:
+    # ### Solo borramos 'programa'. No borres 'tipo_programa' aquí porque se encarga la otra migración ###
+    op.drop_index(op.f('ix_programa_id_programa'), table_name='programa')
+    op.drop_table('programa')
